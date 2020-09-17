8f11aa73d66a5fabab6fba784c27b8df3a7bdd5b
Deswik <59542254+Kirill-Klimenok@users.noreply.github.com>
2020-09-17 23:10:21 +0300

    Update Task_1.java

--- Task_1/Task_1.java
+++ Task_1/Task_1.java
@@ -1,15 +1,15 @@
 package com.company.Task_1;
 
 public class Task_1 {
-    private double z,a,b,c;
+    private double z, a, b, c;
 
 
-    public double getZ(double a,double b ,double c) {
-        this.a=a;
-        this.b=b;
-        this.c=c;
+    public double getZ(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
 
-        this.z=((a-3)*b/2)+c;
+        this.z = ((a - 3) * b / 2) + c;
         System.out.println("Task 1");
         return z;
 
