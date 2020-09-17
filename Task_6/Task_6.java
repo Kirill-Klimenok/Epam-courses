f7bc8a19c05321e996778ddb1444ae641b41622b
Deswik <59542254+Kirill-Klimenok@users.noreply.github.com>
2020-09-17 22:43:54 +0300

    Add files via upload

---
+++ Task_6/Task_6.java
@@ -0,0 +1,12 @@
+package com.company.Task_6;
+
+public class Task_6 {
+    public boolean chart(int x, int y) {
+        boolean result;
+        if (x >= -4 && x < +4&&y>=-3&&y<=4) {
+            result = true;
+
+        } else result = false;
+        return result;
+    }
+}
